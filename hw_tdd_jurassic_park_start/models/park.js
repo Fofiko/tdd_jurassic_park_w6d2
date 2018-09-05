4faf4fabc9cbd4397991ca97b4f52c8bcbf113df
Fofiko <foteinid@gmail>
2018-09-05 07:22:25 +0100

    Pass park has dinosaurs test

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -1,6 +1,7 @@
-const Park = function (name, ticket) {
+const Park = function (name, ticket, dinosaurs) {
   this.name = name;
   this.ticket = ticket;
+  this.dinosaurs = dinosaurs;
 }
 
 module.exports = Park;
--- hw_tdd_jurassic_park_start/specs/park_spec.js
+++ hw_tdd_jurassic_park_start/specs/park_spec.js
@@ -26,7 +26,10 @@ describe('Park', function() {
     assert.strictEqual(actual, 10);
   });
 
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function () {
+    const actual = park.dinosaurs;
+    assert.deepStrictEqual(actual, dinosaurs);
+  });
 
   it('should be able to add a dinosaur to its collection');
 
